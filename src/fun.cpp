82398050a69f7f40afd5811ccce436b2d621b84a
youngtr4der <105950185+youngtr4der@users.noreply.github.com>
2023-03-15 15:34:14 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -3,21 +3,20 @@
 #include <cstring>
 
 unsigned int faStr1(const char *str) {
-    bool f1, f2 = false;
+    bool f1, f2 = true;
     int k = 0;
 
     for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] <= '9' && '0' <= str[i]) {
-            f1 = true;
+        if (str[i] <= '9' && str[i] >= '0') {
+            f1 = false;
         }
         if (str[i] != ' ') {
-            f2 = true;
-        }
-        if (f2 == true && str[i] == ' ') {
-            if (f1 == false) {
+            f2 = false;
+        } else {
+            if (f1 == true && f2 == false ) {
                 k++;
             }
-            f1, f2 = false;
+            f1, f2 = true;
         }
     }
 
@@ -32,15 +31,15 @@ unsigned int faStr2(const char *str) {
         if (space != false && str[i] <= 'Z'; str[i] >= 'A') {
             correct = true;
         }
-        if (str[i] <= 'z' && str[i] >= 'A')
+        if (str[i] <= 'z' && str[i] >= 'A') {
             space = false;
-            else if (str[i] != ' ')
+        } else if (str[i] != ' ') {
                 correct = false;
-                else {
-                    if (correct == true)
-                        k++;
-                }
-                correct = false, space = true;
+        } else {
+            if (correct == true)
+                k++;
+            }
+        correct = false, space = true;
     }
 
     return k;
