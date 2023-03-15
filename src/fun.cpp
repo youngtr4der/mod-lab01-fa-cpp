7f337a01468d690384c663dee7eb09b450399d74
youngtr4der <105950185+youngtr4der@users.noreply.github.com>
2023-03-15 15:14:36 +0300

    rel faStrr1

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,7 +2,25 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    bool f1, f2 = false;
+    int k = 0;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] <= '9' && '0' <= str[i]) {
+            f1 = true;
+        }
+        if (str[i] != ' ') {
+            f2 = true;
+        }
+        if (f2 == true && str[i] == ' ') {
+            if (f1 == false) {
+                k++;
+            }
+            f1, f2 = false;
+        }
+    }
+
+    return k;
 }
 
 unsigned int faStr2(const char *str) {
